ce848e3bdc5f2e403628f51af7666852bb24cf92
carlbalslev <carlbalslev@yahoo.com>
2015-03-10 09:53:22 +0100

    init1

--- black_scholes.m
+++ black_scholes.m
@@ -3,7 +3,7 @@ function result = black_scholes(s0, dt, r, sigma, dW)
 # take one step. The random step is predefined.
 #
  
-st = s0 * exp(r*dt + 0.5*sqrt(dt)*sigma*dW);
-result = st;
+st1 = s0 * exp(r*dt + 0.5*sqrt(dt)*sigma*dW);
+result = st1;
 return
 
